4d29a17bf9d0072472266f3a1822eb5df5051098
李程 <lichengdongdong@qq.com>
2018-07-11 17:46:51 +0800

    t

--- api/config.php
+++ api/config.php
@@ -1,10 +1,10 @@
 <?php
 
+define('DATABASE_HOST', 'ywriyorsfhcj.rds.sae.sina.com.cn:13089'); // localhost
 
- define('DATABASE_HOST', 'ywriyorsfhcj.rds.sae.sina.com.cn:13089'); // localhost
-
-
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers:x-requested-with,content-type");
 
 //
 define('DATABASE_USER', 'lcpapp'); // root
